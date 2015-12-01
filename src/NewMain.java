5312b553baa328d7d5edaa3bb842fac704878aa9
hashcraf <hashcraf@ucsc.edu>
2015-11-30 21:48:56 -0800

    No message

--- src/NewMain.java
+++ src/NewMain.java
@@ -13,6 +13,10 @@ public class NewMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+      
+      Scanner in = new Scanner (new File("test.txt");
+      
+      
         // TODO code application logic here
     }
     
